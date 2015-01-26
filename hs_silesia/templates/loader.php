130ca635dfb313cdf1fb0385ebeb61ac32269d25
Firemark <marpiechula@gmail.com>
2015-01-26 01:47:59 +0100

    correct loader (<3)

--- hs_silesia/templates/loader.php
+++ hs_silesia/templates/loader.php
@@ -1,4 +1,4 @@
 <div class="milky-way--holder" style="display:none;">
   <div class="milky-way"></div>
-  <div class="loader"></div>
+  <div class="loader"><span>Pinky Pie is waiting for a new page</span></div>
 </div>
