97985f7e643f4bab5786441aa079b2ccdd764e02
Dariusz Niemczyk <palid@o2.pl>
2015-02-07 16:17:02 +0100

    A little saner menu... Maybe... I think... NOPE.

--- hs_silesia/functions.php
+++ hs_silesia/functions.php
@@ -100,6 +100,20 @@ function scriptsLoader() {
 
 add_action('wp_enqueue_scripts', 'scriptsLoader');
 
+function getMenu($postlist) {
+  foreach ( $postlist as $post ) {
+   $id = get_the_ID($post);
+     echo sprintf('
+       <li id="post-%s">
+        <a href="%s" rel="bookmark" title="%s">
+          %s
+          <time>%s</time>
+          %s
+        </a>
+      </li>
+      ', $id, get_permalink($id), $post->post_title, get_the_author_meta( 'user_nicename' , $post->post_author ), get_the_time("d.m.y", $id), $post->post_title);
+  }
+}
 
 // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 // DO WE NEED IT?!
--- hs_silesia/templates/news.php
+++ hs_silesia/templates/news.php
@@ -1,21 +1,6 @@
 <div class="col-sm-5 col-sm-offset-1">
   <h3 class='text-center'>Aktualności</h3>
   <ul class='news magic-links'>
-    <?php
-    $postlist = get_posts( 'sort_column=menu_order&sort_order=asc' );
-    $posts = array();
-    foreach ( $postlist as $post ) {
-      $id = get_the_ID($post);
-       echo sprintf('
-         <li id="post-%s">
-          <a href="%s" rel="bookmark" title="%s">
-            %s
-            <time>%s</time>
-            %s
-          </a>
-        </li>
-        ', $id, get_permalink($id), $post->post_title, get_the_author_meta( 'user_nicename' , $post->post_author ), get_the_time("d.m.y", $id), $post->post_title);
-    }
-    ?>
+    <?php getMenu(get_posts( 'sort_column=menu_order&sort_order=asc' )); ?>
   </ul>
 </div>
