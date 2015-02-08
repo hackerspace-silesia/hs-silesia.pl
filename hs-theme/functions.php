a7a8859554d897990542196e6fae8e9b2bad21ca
Dariusz Niemczyk <palid@o2.pl>
2015-02-08 17:57:56 +0100

    Add font awesome

--- hs-theme/functions.php
+++ hs-theme/functions.php
@@ -3,4 +3,6 @@
 wp_register_script('magic-loader', get_stylesheet_directory_uri() . '/js/magic-loader.js', array('jquery'));
 wp_enqueue_script('magic-loader');
 
+wp_enqueue_style( 'font-awesome' , get_stylesheet_directory_uri() . '/css/font-awesome.min.css', null, '4.3.0', 'all');
+
 ?>
