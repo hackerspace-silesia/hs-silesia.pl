71f4330fac3cb1547761bdf63986860ccb1565d8
Firemark <marpiechula@gmail.com>
2015-02-11 01:07:05 +0100

    remove pony (;_;)

--- hs-theme/functions.php
+++ hs-theme/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-wp_register_script('magic-loader', get_stylesheet_directory_uri() . '/js/magic-loader.js', array('jquery'));
-wp_enqueue_script('magic-loader');
+#wp_register_script('magic-loader', get_stylesheet_directory_uri() . '/js/magic-loader.js', array('jquery'));
+#wp_enqueue_script('magic-loader');
 
 ?>
